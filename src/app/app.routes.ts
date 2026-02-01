625376a8bf7e1db8b3631dbbc840fe6ce1308623
Leonel Chauca <leonelchauca0@gmail.com>
2026-01-31 22:27:25 -0400

    initialcommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './features/auth/Layout/auth-layout/auth-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayout,
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.auth_routes),
+  }
+
+];
---
+++ src/app/features/auth/Layout/auth-layout/auth-layout.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AuthLayout { }
